921ff78bbfd829f02bea001ccaaacd48fb9ad48b
nuhahaa <65335537+nuhahaa@users.noreply.github.com>
2020-05-28 20:37:54 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,7 @@
 // prettier-ignore
 export default [
-    { author: "Azza Ulin Nuha", quotes: "Whatever happens, keep breathing" },
-    { author: "Azza Ulin Nuha", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "Azza Ulin Nuha", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "Azza Ulin Nuha", quotes: "Need someone to talk about life" },
     { author: "Azza Ulin Nuha", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "Azza Ulin Nuha", quotes: "MISS U, bby" },
     { author: "Azza Ulin Nuha", quotes: "I'm sorry for loving you." },
 ];
